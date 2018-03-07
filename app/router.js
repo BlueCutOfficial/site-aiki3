994bc0b54ce4a24b9101f63c3ab07ba1efb8346b
Marine <marine.dunstetter@people-doc.com>
2018-03-07 21:57:34 +0100

    feat: routes, navbar, first contents

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('aikido');
+  this.route('club');
+  this.route('practice');
+  this.route('links');
+  this.route('stages');
 });
 
 export default Router;
---
+++ tests/integration/components/navbar-arrow-item-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | navbar-arrow-item', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{navbar-arrow-item}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#navbar-arrow-item}}
+        template block text
+      {{/navbar-arrow-item}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
