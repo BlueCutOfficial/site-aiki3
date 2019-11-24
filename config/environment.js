80045cf02307f315ad7350505debc44460840662
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-11-24 15:56:01 +0100

    feat: configure locationType as hash for URLs

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'site-aiki3',
+    modulePrefix: "site-aiki3",
     environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: "/",
+    locationType: "hash",
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -22,20 +22,20 @@ module.exports = function(environment) {
       // when it is created
     },
 
-    'ember-google-maps': {
-      key: 'AIzaSyDam2nPRQoiGNPRAl1bLWpGTjYacbK2mnM',
-      language: 'fr',
-      region: 'FR',
-      protocol: 'https',
-      version: '3.33',
-      libraries: ['geometry', 'places']
+    "ember-google-maps": {
+      key: "AIzaSyDam2nPRQoiGNPRAl1bLWpGTjYacbK2mnM",
+      language: "fr",
+      region: "FR",
+      protocol: "https",
+      version: "3.33",
+      libraries: ["geometry", "places"]
       // client: undefined,
       // channel: undefined,
       // baseUrl: '//maps.googleapis.com/maps/api/js'
     }
   };
 
-  if (environment === 'development') {
+  if (environment === "development") {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -43,22 +43,22 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
-  if (environment === 'test') {
+  if (environment === "test") {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = "none";
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = "#ember-testing";
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
+  if (environment === "production") {
     // Force URL's anchor to load the starting state of the application
     // This is needed because our current host can't support URL Rewriting
-    ENV.APP.locationType = 'hash';
+    ENV.APP.locationType = "hash";
   }
 
   return ENV;
